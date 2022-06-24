be945831ebc6b17452691e91c491dd56c157309c
Javier Granados <hola@javiergranados.dev>
2022-06-24 12:12:01 +0200

    Persist the token and implement its validation

--- src/api/coffeeApi.ts
+++ src/api/coffeeApi.ts
@@ -1,7 +1,23 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const BASE_URL = 'https://coffee-react-native-fh.herokuapp.com/api';
 
 const coffeeApi = axios.create({ baseURL: BASE_URL });
+
+coffeeApi.interceptors.request.use(async (config) => {
+  if (!config) {
+    config = {};
+  }
+  if (!config.headers) {
+    config.headers = {};
+  }
+
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    config.headers['x-token'] = token;
+  }
+  return config;
+});
 
 export default coffeeApi;
--- src/context/auth/AuthContext.tsx
+++ src/context/auth/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthState, AuthContextProps } from './AuthTypes';
 import { authReducer } from './AuthReducer';
 import { LoginData, LoginResponse } from '../../interfaces/login';
@@ -22,6 +23,8 @@ export const AuthProvider = ({ children }: any) => {
     try {
       const response = await coffeeApi.post<LoginResponse>('/auth/login', { correo, password });
       dispatch({ type: 'LOG_IN', payload: { token: response.data.token, user: response.data.usuario } });
+
+      await AsyncStorage.setItem('token', response.data.token);
     } catch (error: any) {
       const msg: string = error.response.data.msg || GENERIC_ERROR_TEXT;
       addError(msg);
@@ -33,6 +36,25 @@ export const AuthProvider = ({ children }: any) => {
   const removeError = () => dispatch({ type: 'REMOVE_ERROR' });
 
   const logOut = () => {};
+
+  const checkToken = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (!token) {
+      return dispatch({ type: 'LOG_OUT' });
+    }
+
+    const response = await coffeeApi.get('/auth');
+    if (response.status !== 200) {
+      return dispatch({ type: 'LOG_OUT' });
+    }
+
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'LOG_IN', payload: { token: response.data.token, user: response.data.usuario } });
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
 
   return (
     <AuthContext.Provider
